db0c47a1c5986366b15794aaf7fb116ccd70eec4
Art <WildandArt@users.noreply.github.com>
2022-06-16 14:11:11 +0300

    Initial Commit

---
+++ Programmatic TableView with DiffableDataSource and FlowlAyout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic TableView with DiffableDataSource and FlowlAyout
+//
+//  Created by Artemy Ozerski on 16/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
